d0506c7f081fd7c17032f38184ff66a1334b08e8
Eder Braz Velludo <edervelludo@gmail.com>
2022-03-25 11:02:05 -0300

    first

---
+++ src/Api.Data/Context/ContextFactory.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Api.Data.Context
+{
+    public class ContextFactory : IDesignTimeDbContextFactory<MyContext>
+    {
+        public MyContext CreateDbContext(string[] args)
+        {
+            //Usado para Criar as Migrações
+            var connectionString = "Server=127.0.0.1;Port=3306;DataBase=curso_api_netcore;Uid=root;Pwd=gX^J4yMS";
+            var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
+            optionsBuilder.UseMySql(connectionString);
+            return new MyContext(optionsBuilder.Options);
+        }
+    }
+}
